65c1db94c48991a1bcca096ecebcdfb26c6e4701
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-31 22:50:48 +0900

    :pencil2:  Export functions

--- validation/mod.ts
+++ validation/mod.ts
@@ -1,2 +1,17 @@
 // Copyright 2021-present the is-valid authors. All rights reserved. MIT license.
 export * from "./length.ts";
+export * from "./isArray.ts";
+export * from "./isBigint.ts";
+export * from "./isBoolean.ts";
+export * from "./isEmpty.ts";
+export * from "./isFunction.ts";
+export * from "./isJSONObject.ts";
+export * from "./isLength0.ts";
+export * from "./isNil.ts";
+export * from "./isNull.ts";
+export * from "./isNumber.ts";
+export * from "./isObject.ts";
+export * from "./isPrimitive.ts";
+export * from "./isString.ts";
+export * from "./isSymbol.ts";
+export * from "./isUndefined.ts";
